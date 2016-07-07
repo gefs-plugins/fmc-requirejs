408c7bb3bcc5066cd0308964b36bf7c4ce9af36b
Harry Xue <harry.xue@hotmail.com>
2016-07-07 16:21:40 +0800

    Added UI elements for redesign

---
+++ source/ui/main.js
@@ -0,0 +1,5 @@
+"use strict"; /*jshint unused:false*/
+
+define(['lib', 'log', 'toggle', 'waypoints'], function (lib, log, toggle, waypoints) {
+	$('.fmc-toggle-button').addClass('mdl-color--green-green mdl-color-text--white-white');
+});
