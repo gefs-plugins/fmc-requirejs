60f490daef196d8b4711a5068afdb0c6ecdb0d60
Harry Xue <harry.xue@hotmail.com>
2016-07-10 11:52:55 +0800

    Enabled local testing

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 ### fmc-requirejs
 This is the RequireJS implementation of the Flight Management Computer for GEFS Online
+
+
+----
+#### Testing
+This allows you to run a server for GEFS from your local plugin files.
+
+Before running this, you'll need to install [Node.js](http://nodejs.org).
+
+After doing that, open a terminal/command prompt in this directory:
+
+ - `npm install`
+ - `npm start`
+
+Then you will be able to access the game at <http://127.0.0.1:3000/gefs.php>
---
+++ testing/index.js
@@ -0,0 +1,36 @@
+"use strict";
+
+// jshint node:true
+var heads = require('robohydra').heads;
+var RoboHydraHeadFilesystem = heads.RoboHydraHeadFilesystem;
+var RoboHydraHeadProxy = heads.RoboHydraHeadProxy;
+var RoboHydraHeadFilter = heads.RoboHydraHeadFilter;
+var path = require('path');
+var script = '<script async defer data-main="/fmc/main" src="/fmc/require.js"></script></head>';
+
+exports.getBodyParts = function (conf) {
+  return {
+    heads: [
+      new RoboHydraHeadFilesystem({
+        mountPath: '/fmc',
+        documentRoot: path.join(__dirname, '../source')
+      }),
+      
+      new RoboHydraHeadFilter({
+        path: '/gefs.php*',
+        filter: function (buffer) {
+          return buffer.toString().replace('</head>', script);
+        }
+      }),
+
+      new RoboHydraHeadProxy({
+        mountPath: '/',
+        proxyTo: 'http://www.gefs-online.com',
+        setHostHeader: true
+      })
+    ]
+  };
+};
+
+console.log('Please go to http://127.0.0.1:3000/gefs.php to start Cesium-GEFS.');
+console.log('To exit, press Ctrl+C or close this window.');
