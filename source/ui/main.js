87da7849892a51d7989ad5e31c4277201562c752
Harry Xue <harry.xue@hotmail.com>
2016-07-12 23:34:37 +0800

    WIP in source/ui/ ; minor cleanups

--- source/ui/main.js
+++ source/ui/main.js
@@ -5,17 +5,30 @@ function (consts, distance, lib, log, math, toggles, waypoints, lnav, progress, 
 
 	/* ---- UI actions binding ---- */
 
+	// FMC Modal: open and close features
+	var modal = document.getElementsByClassName('fmc-modal')[0];
+    var button = $('.fmc-btn');
 
+    button.click(function () {
+    	if (!modal.open) modal.showModal();
+		else modal.close();
+    });
+
+    $(modal).find('.close').click(function () {
+		modal.close();
+	});
+	// ----
 
 	/* ---- All Initializations ---- */
 
 	// Initializes all timers
-	lnav.timer = setInterval(lnav.update, 5000);
+	// FIXME It is annoying when debugging
+	/*lnav.timer = setInterval(lnav.update, 5000);
 	progress.timer = setInterval(progress.update, 5000);
 	log.mainTimer = setInterval(log.update, 120000);
 	log.gearTimer = setInterval(log.gear, 12000);
 	log.flapsTimer = setInterval(log.flaps, 5000);
-	log.speedTimer = setInterval(log.speed, 15000);
+	log.speedTimer = setInterval(log.speed, 15000);*/
 
 	// Adds one input field on start
 	waypoints.addWaypoint();
