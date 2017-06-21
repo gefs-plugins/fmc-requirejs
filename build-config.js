a633d20cb4701357ec3a3a1c1f2860bdc7454ac8
harryxue1999 <harry.xue@hotmail.com>
2017-06-21 21:37:57 +0800

    Revised optimization main file

--- build-config.js
+++ build-config.js
@@ -1,7 +1,7 @@
 ({
     baseUrl: "./source",
     name: "../node_modules/requirejs/require",
-    include: "main",
+    include: "init",
     mainConfigFile: "./source/config.js",
     out: "./build/fmc.min.js",
     optimize: "uglify2"
