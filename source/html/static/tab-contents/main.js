8a2f7b922f2ac09d74358d517d48c0f830389b77
harryxue1999 <harry.xue@hotmail.com>
2017-07-04 14:05:10 +0800

    Added initial components for LEGS and ILS pages

--- source/html/static/tab-contents/main.js
+++ source/html/static/tab-contents/main.js
@@ -1,9 +1,9 @@
 "use strict";
 
 define([
-    'minify!./route.html', 'minify!./arr.html', 'minify!./vnav.html',
-    'minify!./progress.html', 'minify!./map.html', 'minify!./load.html',
-    'minify!./log.html'
-], function (route, arr, vnav, progress, map, load, log) {
-    return [route, arr, vnav, progress, map, load, log].join('');
+    'minify!./route.html', 'minify!./arr.html', 'minify!./legs.html',
+    'minify!./vnav.html', 'minify!./ils.html', 'minify!./progress.html',
+    'minify!./map.html', 'minify!./load.html', 'minify!./log.html'
+], function (route, arr, legs, vnav, ils, progress, map, load, log) {
+    return [route, arr, legs, vnav, ils, progress, map, load, log].join('');
 });
--- source/style/main.js
+++ source/style/main.js
@@ -3,14 +3,14 @@
 define([
     'minify!./button.css', 'minify!./externaldist.css', 'minify!./modal.css',
     'minify!./route.css', 'minify!./waypoints.css', 'minify!./arr.css',
-    'minify!./vnav.css', 'minify!./progress.css', 'minify!./map.css',
-    'minify!./load.css', 'minify!./log.css'
+    'minify!./legs.css', 'minify!./vnav.css', 'minify!./ils.css',
+    'minify!./progress.css', 'minify!./map.css', 'minify!./load.css', 'minify!./log.css'
 ], function (
     button, externalDist, modal, route, waypoints,
-    arr, vnav, progress, map, load, log
+    arr, legs, vnav, ils, progress, map, load, log
 ) {
     return [
         button, externalDist, modal, route, waypoints,
-        arr, vnav, progress, map, load, log
+        arr, legs, vnav, ils, progress, map, load, log
     ].join('');
 });
