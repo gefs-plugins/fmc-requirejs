f5c95d629e810793526e3e6e04072ead1f4207a0
harryxue1999 <harry.xue@hotmail.com>
2017-07-18 15:31:16 +0800

    Added object to access compiled data in-game

---
+++ source/data.js
@@ -0,0 +1,31 @@
+"use strict";
+
+define(['exports'], function (exports) {
+
+    var FILE_PATH = PAGE_PATH + 'fmc/compile/compiled-data/'
+
+    $.get(FILE_PATH + 'waypoints.json', function (waypoints) {
+        exports.waypoints = waypoints;
+    });
+
+    $.get(FILE_PATH + 'navaids.json', function (navaids) {
+        exports.navaids = navaids;
+    });
+
+    $.get(FILE_PATH + 'SID.json', function (SID) {
+        exports.SID = SID;
+    });
+
+    $.get(FILE_PATH + 'STAR.json', function (STAR) {
+        exports.STAR = STAR;
+    });
+
+    $.get(FILE_PATH + 'ATS.json', function (ATS) {
+        exports.ATS = ATS;
+    });
+
+    $.get(FILE_PATH + 'runways.json', function (runways) {
+        exports.runways = runways;
+    });
+
+});
