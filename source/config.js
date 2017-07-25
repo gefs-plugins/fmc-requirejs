3f21efe573032e0c5ddd34752c998b4a96685549
Harry Xue <harry.xue@hotmail.com>
2017-07-25 11:29:32 +0800

    Changed knockout.js dependency back to minified production version

--- source/config.js
+++ source/config.js
@@ -4,7 +4,7 @@
     requirejs.config({
     	urlArgs: '_=' + Date.now(), // Cache bust
     	paths: {
-            knockout: '../node_modules/knockout/build/output/knockout-latest.debug',
+            knockout: '../node_modules/knockout/build/output/knockout-latest',
     		text: '../node_modules/text/text',
     		minify: '../node_modules/minify/minify'
     	}
