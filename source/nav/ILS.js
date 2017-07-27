06ac300f4abcca42981394ad657bf71f93c07267
Harry Xue <harry.xue@hotmail.com>
2017-07-27 11:32:22 +0800

    Revising ILS function

--- source/nav/ILS.js
+++ source/nav/ILS.js
@@ -1,24 +1,24 @@
 "use strict";
 
-define(['math', 'exports'], function (math, exports) {
+define(['flight', 'utils', 'exports'], function (flight, utils, exports) {
 
-    // Glideslope, in degrees (default = 3)
-    exports.glideslope = 3;
+    // Glideslope, in degrees
+    var glideslope = flight.arrival.runway()[3];
 
     // Threshold lat/lon
-    exports.thresholdCoords = [];
+    var thresholdCoords = [ flight.arrival.runway()[0], flight.arrival.runway()[1] ];
 
     // Opposite end lat/lon
-    exports.oppositeCoords = [];
+    // FIXME: find thresholdCoords of opposite runway
+    // | Opposite runway # - Current runway # | = 18, L <-> R, C <-> C
+    var oppositeCoords = [];
 
     // URLs of instruments
-    var dev = true; // Temporary
-    var DEV_BASE = '/fmc/images/';
-    var BASE = '';
+    var FILE_PATH = PAGE_PATH + 'fmc/images/';
     var url = {
-        attitudeJetILS: PAGE_PATH + dev ? DEV_BASE : BASE + 'attitude-jet-ils.png',
-        glideslopeHand: PAGE_PATH + dev ? DEV_BASE : BASE + 'glideslope-indicator.png',
-        localizerHand: PAGE_PATH + dev ? DEV_BASE : BASE + 'localizer-indicator.png'
+        attitudeJetILS: FILE_PATH + 'attitude-jet-ils.png',
+        glideslopeHand: FILE_PATH + 'glideslope-indicator.png',
+        localizerHand: FILE_PATH + 'localizer-indicator.png'
     };
 
     /**
@@ -27,7 +27,7 @@ define(['math', 'exports'], function (math, exports) {
      * @returns {Number} The value of glideslope deviation
      */
     function glideslopeCalc () { // FIXME find the true value for "VALUE"
-        var gs = exports.glideslope;
+        var gs = glideslope;
         var threshold = exports.thresholdCoords;
         var current = geofs.aircraft.instance.llaLocation;
 
@@ -38,11 +38,11 @@ define(['math', 'exports'], function (math, exports) {
 
         // Landing zone calculations and target altitude
         var altitudeAtAim = geofs.getGroundAltitude(threshold[0], threshold[1]); // Feet
-        var distanceToAim = math.getDistance(current[0], current[1], threshold[0], threshold[1]) + 500 * math.FEET_TO_NM; // nm
-        var targetAltitude = Math.tan(math.toRadians(gs)) * distanceToAim * math.NM_TO_FEET + altitudeAtAim; // Feet
+        var distanceToAim = utils.getDistance(current[0], current[1], threshold[0], threshold[1]) + 500 * utils.FEET_TO_NM; // nm
+        var targetAltitude = Math.tan(utils.toRadians(gs)) * distanceToAim * utils.NM_TO_FEET + altitudeAtAim; // Feet
 
         // Each tick stands for 1/4 degrees off the intended glideslope
-        var quarterDegree = Math.tan(math.toRadians(0.25)) * distanceToAim * math.NM_TO_FEET;
+        var quarterDegree = Math.tan(utils.toRadians(0.25)) * distanceToAim * utils.NM_TO_FEET;
         var ticksOff = (targetAltitude - current[2]) / quarterDegree;
 
         // Maximum of 2 ticks on each side
@@ -57,8 +57,8 @@ define(['math', 'exports'], function (math, exports) {
      * @returns {Number} The value of localizer deviation
      */
     function localizerCalc () { // FIXME find the true value for "VALUE" and offset distance threshold
-        var threshold = exports.thresholdCoords;
-        var opposite = exports.oppositeCoords;
+        var threshold = thresholdCoords;
+        var opposite = oppositeCoords;
         var current = geofs.aircraft.instance.llaLocation;
 
         // If either one component is missing, invalid, returns 0
@@ -67,13 +67,13 @@ define(['math', 'exports'], function (math, exports) {
         var VALUE = 1; // Constant to multiply ticks off by
 
         // Deviation from centerline calculation, achieved by using the sine of the angle in between
-        var distanceToThreshold = math.getDistance(current[0], current[1], threshold[0], threshold[1]);
-        var runwayBearing = math.getBearing(threshold[0], threshold[1], opposite[0], opposite[1]);
-        var deltaTheta = math.getBearing(current[0], current[1], threshold[0], threshold[1]) - runwayBearing;
-        var offsetDistance = Math.sin(math.toRadians(deltaTheta)) * distanceToThreshold * math.NM_TO_FEET;
+        var distanceToThreshold = utils.getDistance(current[0], current[1], threshold[0], threshold[1]);
+        var runwayBearing = utils.getBearing(threshold[0], threshold[1], opposite[0], opposite[1]);
+        var deltaTheta = utils.getBearing(current[0], current[1], threshold[0], threshold[1]) - runwayBearing;
+        var offsetDistance = Math.sin(utils.toRadians(deltaTheta)) * distanceToThreshold * utils.NM_TO_FEET;
 
         // Each tick stands for 3 degrees off the centerline (300 feet at threshold)
-        var maxOffsetDistance = Math.sin(math.toRadians(3)) * (distanceToThreshold * math.NM_TO_FEET + 500);
+        var maxOffsetDistance = Math.sin(utils.toRadians(3)) * (distanceToThreshold * utils.NM_TO_FEET + 500);
         var ticksOff = offsetDistance / (300 + maxOffsetDistance);
 
         // Maximum of 1 tick on each side
