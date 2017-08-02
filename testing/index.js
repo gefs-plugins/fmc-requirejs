a9a82ff0a06a0b3cb4b982aace21b1a9128ed9ae
Harry Xue <harry.xue@hotmail.com>
2017-08-02 10:32:55 +0800

    Added cache bust for config file

--- testing/index.js
+++ testing/index.js
@@ -7,7 +7,7 @@ var RoboHydraHeadFilter = heads.RoboHydraHeadFilter;
 var RoboHydraHeadProxy = heads.RoboHydraHeadProxy;
 var path = require('path');
 var script = '<script data-main="fmc/source/init" src="/fmc/node_modules/requirejs/require.js"></script>\
-	<script src="fmc/source/config.js"></script></head>';
+	<script src="fmc/source/config.js?_=' + Date.now() + '"></script></head>';
 
 exports.getBodyParts = function () {
 	return {
