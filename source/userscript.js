d867d64dcfb5b98ff3a4ba3d2e1120d16c01fcb2
Harry Xue <harry.xue@hotmail.com>
2018-03-22 11:17:05 -0500

    Changed from http to https ; update FMC to v0.5.5-m1

--- source/userscript.js
+++ source/userscript.js
@@ -3,8 +3,8 @@
 // @description This extension adds an FMC which controls features such as VNAV, LNAV, route, progress information, etc.
 // @author Harry Xue and Ethan Shields
 // @namespace GeoFS-Plugins
-// @match http://*/geofs.php*
+// @match https://*/geofs.php*
 // @run-at document-end
-// @version 0.5.5
+// @version 0.5.5-m1
 // @grant none
 // ==/UserScript==
